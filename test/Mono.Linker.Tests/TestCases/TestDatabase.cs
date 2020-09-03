92174fd7ad4c00b23c09e516d8c495df8c86b30b
Michael Voorhees <michaelv@unity3d.com>
2020-09-03 15:08:13 -0400

    Revert "Update to NetCore 3.1"
    This reverts commit 28cbdb342902259811aae7ffecdf873ec4d88ec2.

--- test/Mono.Linker.Tests/TestCases/TestDatabase.cs
+++ test/Mono.Linker.Tests/TestCases/TestDatabase.cs
@@ -212,8 +212,6 @@ namespace Mono.Linker.Tests.TestCases
 
 #if NETCOREAPP3_0
 			var tfm = "netcoreapp3.0";
-#elif NETCOREAPP3_1
-			var tfm = "netcoreapp3.1";
 #elif NET471
 			var tfm = "net471";
 #else
