9c8ebc020e7206edbc959fee86cddd27048bdbd3
Michael Voorhees <michaelv@unity3d.com>
2020-10-16 14:23:33 -0400

    Stub void return ETW bodies instead of throw
    It is somewhat common for the ETW pattern to be incorrectly implemented.  For example the `IsEnabled` check can be inside of the method on the EventSource type.  When we replace the body with a throw this leads to
    runtime exceptions.  Let's just clear the bodies of the methods on the EventSource type when the return type is void.  For non-void returning methods let's continue to add a throw since replacing the body with something like `return null` could result in unexpected errors later down the line.

--- test/Mono.Linker.Tests.Cases/BCLFeatures/ETW/Excluded.cs
+++ test/Mono.Linker.Tests.Cases/BCLFeatures/ETW/Excluded.cs
@@ -35,9 +35,7 @@ namespace Mono.Linker.Tests.Cases.BCLFeatures.ETW {
 		[Kept]
 		[ExpectedInstructionSequence (new []
 		{
-			"ldstr",
-			"newobj",
-			"throw",
+			"ret",
 		})]
 		protected override void OnEventCommand (EventCommandEventArgs command)
 		{
@@ -51,9 +49,7 @@ namespace Mono.Linker.Tests.Cases.BCLFeatures.ETW {
 		[Kept]
 		[ExpectedInstructionSequence (new []
 		{
-			"ldstr",
-			"newobj",
-			"throw",
+			"ret",
 		})]
 		[Event (8)]
 		public void SomeMethod ()
